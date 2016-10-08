dfa0e596e9e1d6d266ab2bbb1e531a4bca3bd54e
starkhorn <t.earendel@gmail.com>
2016-10-08 23:53:43 +0700

    Display markdown editor.

---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,44 @@
+<style scope>
+  #editor td {
+    vertical-align: top;
+  }
+</style>
+
+<template>
+  <div>
+    <table id="editor">
+      <tbody>
+        <tr>
+          <td>
+            <textarea cols="100" rows="20" v-model="rawContent">
+            </textarea>
+          </td>
+          <td>
+            <div v-html="renderedContent"></div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <button @click="preview">Preview</button>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data () {
+      return {
+        rawContent: '',
+        renderedContent: ''
+      }
+    },
+
+    methods: {
+      preview () {
+        // TODO: transform raw content to rendered
+        this.renderedContent = this.rawContent
+      }
+    }
+  }
+</script>
